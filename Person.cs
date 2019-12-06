eb9bf8e0bb600a2bcbfedb89158f99a9dcdcf314
LuayRahil02 <54432462+LuayRahil02@users.noreply.github.com>
2019-12-06 12:30:55 +0100

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lektion_10___luay
+{
+    class Person
+    {
+        private string namn;
+        private int ålder;
+        private string adress;
+
+        public string Namn
+        {
+            get { return namn; }
+            set { namn = value; }
+        }
+        public int Ålder
+        {
+            get { return ålder; }
+            set { ålder = value; }
+        }
+        public string Adress
+        {
+            get { return adress; }
+            set { adress = value; }
+        }
+
+        public Person(string n, int å, string a)
+        {
+            Namn = n;
+            Ålder = å;
+            Adress = a;
+        }
+        public Person() { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lektion_10___luay
+{
+    class Program
+    {
+        public static void Main()
+        {
+            List<Person> personlista = new List<Person>();
+
+            Person p = new Person("Luke Skywalker", 20, "Moisture fram 1138, Tatooine");
+            personlista.Add(p);
+            personlista.Add(new Person("Lando Calrissian", 28, "Cloud City, Bespin"));
+
+            foreach (Person element in personlista)
+            {
+                Console.WriteLine(element.Namn);
+                Console.WriteLine(element.Ålder);
+                Console.WriteLine(element.Adress);
+            }
+
+
+        }
+    }
+}
